3e18049d5587e00f4d9776a257ac57d05d335224
bhagyajanadhi <janadhibhagya3@gmail.com>
2024-04-27 14:14:25 +0530

    first commit

---
+++ src/main/java/lk/ijse/controller/BookingManagementController.java
@@ -0,0 +1,8 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+
+public class BookingManagementController {
+    public void handleAddButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/db/Dbconnection.java
@@ -0,0 +1,21 @@
+package lk.ijse.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbconnection {
+    private static Dbconnection dbconnection;
+    private Connection connection;
+    private Dbconnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/garage_pro","root","Ijse@123");
+    }
+
+    public static Dbconnection getInstance() throws SQLException {
+        return (null==dbconnection) ? dbconnection = new Dbconnection(): dbconnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
+
